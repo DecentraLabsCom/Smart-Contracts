ed9e464cc6c97b20b5fb34598a9886fc817b6344
Juan Luis Ramos <juanluis.ramosv@gmail.com>
2025-04-18 10:43:23 +0200

    Documentation changed for NatSpec compatibility

--- contracts/facets/ProviderFacet.sol
+++ contracts/facets/ProviderFacet.sol
@@ -1,20 +1,13 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 pragma solidity ^0.8.23;
 
-/**
- * @title ProviderFacet
- * @author Juan Luis Ramos Villalón
- * @author Luis de la Torre Cubillo
- * @dev This contract is a facet of a diamond that handles admin and provider roles.
- *      It inherits from AccessControlUpgradeable to manage role-based access control.
- *      Ensure to initialize the AccessControlUpgradeable properly when deploying or upgrading.
- */
-
-
-/**
- * @dev Defines the initial amount of LAB tokens assigned to a new entity.
- * This constant is used to set the starting balance for LAB tokens.
- */
+// @title ProviderFacet
+// @author
+// - Juan Luis Ramos Villalón
+// - Luis de la Torre Cubillo
+// @dev This contract is a facet of a diamond that handles admin and provider roles.
+//      It inherits from AccessControlUpgradeable to manage role-based access control.
+//      Ensure to initialize the AccessControlUpgradeable properly when deploying or upgrading.
 
 uint32 constant INITIAL_LAB_TOKENS = 1000;
 
@@ -22,189 +15,148 @@ import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol"
 import {LibAppStorage, AppStorage, PROVIDER_ROLE, Provider, ProviderBase} from "../libraries/LibAppStorage.sol";
 import "../libraries/LibDiamond.sol";
 import {LibAccessControlEnumerable} from "../libraries/LibAccessControlEnumerable.sol";
-import "../external/LabERC20.sol";  // Import the LabERC20 contract, no yet implemented
-
+import "../external/LabERC20.sol"; // Import the LabERC20 contract, no yet implemented
 
 contract ProviderFacet is AccessControlUpgradeable {
-     
-     using LibAccessControlEnumerable for AppStorage;
+    using LibAccessControlEnumerable for AppStorage;
 
-     /**
-      * @dev Emitted when a new provider is added to the system.
-      * @param account The address of the provider being added.
-      * @param name The name of the provider.
-      * @param email The email address of the provider.
-      * @param country The country of the provider.
-      */
-     event ProviderAdded(address indexed account, string name, string email, string country);
+    /// @dev Emitted when a new provider is added to the system.
+    /// @param _account The address of the provider being added.
+    /// @param _name The name of the provider.
+    /// @param _email The email address of the provider.
+    /// @param _country The country of the provider.
+    event ProviderAdded(
+        address indexed _account,
+        string _name,
+        string _email,
+        string _country
+    );
 
-     /**
-      * @dev Emitted when a provider is removed.
-      * @param account The address of the provider that was removed.
-      */
-     event ProviderRemoved(address indexed account);
+    /// @dev Emitted when a provider is removed.
+    /// @param _account The address of the provider that was removed.
+    event ProviderRemoved(address indexed _account);
 
-     /**
-      * @dev Emitted when a provider's information is updated.
-      * @param account The address of the provider.
-      * @param name The name of the provider.
-      * @param email The email address of the provider.
-      * @param country The country of the provider.
-      */
-     event ProviderUpdated(address indexed account, string name, string email, string country);
+    /// @dev Emitted when a provider's information is updated.
+    /// @param _account The address of the provider.
+    /// @param _name The name of the provider.
+    /// @param _email The email address of the provider.
+    /// @param _country The country of the provider.
+    event ProviderUpdated(
+        address indexed _account,
+        string _name,
+        string _email,
+        string _country
+    );
 
+    /// @dev Modifier to restrict access to functions that can only be executed by accounts
+    ///      with the `DEFAULT_ADMIN_ROLE`. Ensures that the caller of the function has the
+    ///      required role before proceeding with the execution of the function.
+    /// @notice Reverts if the caller does not have the `DEFAULT_ADMIN_ROLE`.
+    modifier defaultAdminRole() {
+        require(
+            hasRole(DEFAULT_ADMIN_ROLE, msg.sender),
+            "Only the default admin can perform this action"
+        );
+        _;
+    }
 
-     /**
-      * @dev Modifier to restrict access to functions that can only be executed by accounts
-      *      with the `DEFAULT_ADMIN_ROLE`. Ensures that the caller of the function has the
-      *      required role before proceeding with the execution of the function.
-      *
-      * Requirements:
-      * - The caller must have the `DEFAULT_ADMIN_ROLE`.
-      *
-      * Reverts:
-      * - If the caller does not have the `DEFAULT_ADMIN_ROLE`, the transaction is reverted
-      *   with the error message: "Only the default admin can perform this action".
-      */
-     modifier defaultAdminRole() {
-          require(hasRole(DEFAULT_ADMIN_ROLE, msg.sender), "Only the default admin can perform this action");
-          _;
-     }
+    /// @dev Constructor for the `ProviderFacet` contract.
+    /// This constructor is intentionally left empty.
+    constructor() {}
 
+    /// @dev Initializes the contract setting up the initial admin role and other parameters.
+    /// @notice The caller must be the contract owner.
+    /// @param _name The name of the initial admin.
+    /// @param _email The email of the initial admin.
+    /// @param _country The country of the initial admin.
+    /// @param _labERC20 The address of the LabERC20 token contract.
+    function initialize(
+        string memory _name,
+        string memory _email,
+        string memory _country,
+        address _labERC20
+    ) public initializer {
+        LibDiamond.enforceIsContractOwner();
+        bool granted = _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        if (granted) _s()._addProviderRole(msg.sender, _name, _email, _country);
 
-      /**
-      * @dev Constructor for the `ProviderFacet` contract.
-      * This constructor is intentionally left empty.
-      */
-     constructor() {}
+        _s().DEFAULT_ADMIN_ROLE = DEFAULT_ADMIN_ROLE;
+        _s().labTokenAddress = _labERC20;
+    }
 
+    /// @notice Adds a new provider to the system.
+    /// @dev Grants the `PROVIDER_ROLE` to the specified account and initializes the provider's details.
+    ///      Mints `INITIAL_LAB_TOKENS` LabERC20 tokens to the provider's account upon successful addition.
+    ///      Emits a `ProviderAdded` event upon successful addition.
+    ///      Reverts if the provider already exists.
+    /// @param _name The name of the provider.
+    /// @param _account The address of the provider's account.
+    /// @param _email The email address of the provider.
+    /// @param _country The country of the provider.
+    function addProvider(
+        string memory _name,
+        address _account,
+        string memory _email,
+        string memory _country
+    ) external defaultAdminRole {
+        if (_grantRole(PROVIDER_ROLE, _account)) {
+            _s()._addProviderRole(_account, _name, _email, _country);
+            LabERC20(_s().labTokenAddress).mint(_account, INITIAL_LAB_TOKENS);
+            emit ProviderAdded(_account, _name, _email, _country);
+        } else {
+            revert("Provider already exists");
+        }
+    }
 
-     /**
-     * @dev Initializes the contract setting up the initial admin role and other parameters.
-     * 
-     * Requirements:
-     * - The caller must be the contract owner.
-     * 
-     * @param _name The name of the initial admin.
-     * @param _email The email of the initial admin.
-     * @param _country The country of the initial admin.
-     * @param _labERC20 The address of the LabERC20 token contract.
-     * 
-     * @custom:emits Emits a {RoleGranted} event if the role is successfully granted.
-     */
-     function initialize(string memory _name, string memory _email, string memory _country, address _labERC20) public initializer {
-          
-          LibDiamond.enforceIsContractOwner();
-          bool granted = _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
-          if (granted) _s()._addProviderRole(msg.sender, _name, _email, _country);
+    /// @notice Removes a provider from the system by revoking their PROVIDER_ROLE.
+    /// @dev This function can only be called by an account with the DEFAULT_ADMIN_ROLE.
+    ///      If the provider does not have the PROVIDER_ROLE, the transaction reverts with an error.
+    ///      Emits a `ProviderRemoved` event upon successful removal.
+    /// @param _provider The address of the provider to be removed.
+    function removeProvider(
+        address _provider
+    ) external onlyRole(DEFAULT_ADMIN_ROLE) {
+        if (_revokeRole(PROVIDER_ROLE, _provider)) {
+            _s()._removeProviderRole(_provider);
+            emit ProviderRemoved(_provider);
+        } else {
+            revert("Provider does not exist");
+        }
+    }
 
-          _s().DEFAULT_ADMIN_ROLE = DEFAULT_ADMIN_ROLE;
-          _s().labTokenAddress = _labERC20;
-         
-     }
+    /// @notice Updates the provider information for the caller.
+    /// @dev This function allows a provider to update their name, email, and country.
+    ///      Only accounts with the `PROVIDER_ROLE` can call this function.
+    /// @param _name The updated name of the provider.
+    /// @param _email The updated email address of the provider.
+    /// @param _country The updated country of the provider.
+    function updateProvider(
+        string memory _name,
+        string memory _email,
+        string memory _country
+    ) external onlyRole(PROVIDER_ROLE) {
+        _s().providers[msg.sender] = ProviderBase(_name, _email, _country);
+        emit ProviderUpdated(msg.sender, _name, _email, _country);
+    }
 
-   
-     /**
-      * @notice Adds a new provider to the system.
-      * @dev Grants the `PROVIDER_ROLE` to the specified account and initializes the provider's details.
-      *      Mints `INITIAL_LAB_TOKENS` LabERC20 tokens to the provider's account upon successful addition.
-      *      Emits a `ProviderAdded` event upon successful addition.
-      *      Reverts if the provider already exists.
-      * @param _name The name of the provider.
-      * @param _account The address of the provider's account.
-      * @param _email The email address of the provider.
-      * @param _country The country of the provider.
-      * @custom:modifier `defaultAdminRole` - Only callable by accounts with the default admin role.
-      * @custom:reverts Reverts with "Provider already exists" if the account already has the `PROVIDER_ROLE`.
-      * @custom:emits Emits a `ProviderAdded` event with the provider's details.
-      */
-     function addProvider(
-          string memory _name,
-          address _account,
-          string memory _email,
-          string memory _country
-     ) external defaultAdminRole {
-        
-          if(_grantRole(PROVIDER_ROLE, _account)){
-                _s()._addProviderRole(_account, _name, _email, _country);
-                LabERC20(_s().labTokenAddress).mint(_account, INITIAL_LAB_TOKENS); 
-                emit ProviderAdded(_account, _name, _email, _country);
-          }else{
-               revert("Provider already exists");
-          }
-     }
+    /// @notice Checks if the given account is a Lab provider.
+    /// @param _account The address of the account to check.
+    /// @return A boolean indicating whether the account is a Lab provider.
+    function isLabProvider(address _account) external view returns (bool) {
+        return _s()._isLabProvider(_account);
+    }
 
-   
-     /**
-      * @notice Removes a provider from the system by revoking their PROVIDER_ROLE.
-      * @dev This function can only be called by an account with the DEFAULT_ADMIN_ROLE.
-      *      If the provider does not have the PROVIDER_ROLE, the transaction reverts with an error.
-      *      The function emits a `ProviderRemoved` event upon successful removal.
-      * @param _provider The address of the provider to be removed.
-      * @custom:warning Ensure that the provider being removed does not have associated Labs,
-      *                 as this function does not handle such cases. Additional logic may be required.
-      * @custom:emit Emits a `ProviderRemoved` event upon successful removal of the provider.
-      */
-     function removeProvider(address _provider) external onlyRole(DEFAULT_ADMIN_ROLE)  {
-          if (_revokeRole(PROVIDER_ROLE, _provider)){
-               _s()._removeProviderRole(_provider);
-               emit ProviderRemoved(_provider);}
-          else{
-               revert("Provider does not exist");
-          } 
+    /// @notice Retrieves the list of all Lab providers.
+    /// @return An array of LabProviderExt structs representing all Lab providers.
+    function getLabProviders() external view returns (Provider[] memory) {
+        return _s()._getLabProviders();
+    }
 
-          // TODO: What if the provider has Labs?
-     }
-
-
-     /**
-     * @notice Updates the provider information for the caller.
-     * @dev This function allows a provider to update their name, email, and country.
-     *      Only accounts with the `PROVIDER_ROLE` can call this function.
-     * @param _name The updated name of the provider.
-     * @param _email The updated email address of the provider.
-     * @param _country The updated country of the provider.
-     *
-     * Emits an {ProviderUpdated} event.
-     */
-     function updateProvider(
-          string memory _name,
-          string memory _email,
-          string memory _country
-     ) external onlyRole(PROVIDER_ROLE)  {
-          _s().providers[msg.sender] = ProviderBase(_name, _email, _country);
-          emit ProviderUpdated(msg.sender, _name, _email, _country);
-     }
-
- 
-     /**
-      * @notice Checks if the given account is a Lab provider.
-      * @param _account The address of the account to check.
-      * @return A boolean indicating whether the account is a Lab provider.
-      */
-     function isLabProvider(address _account) external view returns (bool) {
-          return _s()._isLabProvider(_account);
-     }
-
-      
-     /**
-      * @notice Retrieves the list of all Lab providers.
-      * @return An array of LabProviderExt structs representing all Lab providers.
-      */
-      function getLabProviders() external view returns (Provider[] memory) {
-          return _s()._getLabProviders();
-     }
-
-
-      /**
-      * @dev Internal pure function to retrieve the application storage structure.
-      *      This function provides access to the `AppStorage` instance by calling
-      *      the `diamondStorage` function from the `LibAppStorage` library.
-      *
-      * @return s The storage instance of type `AppStorage`.
-      */
-     function _s() internal pure returns (AppStorage storage s) {
+    /// @dev Internal pure function to retrieve the application storage structure.
+    ///      This function provides access to the `AppStorage` instance by calling
+    ///      the `diamondStorage` function from the `LibAppStorage` library.
+    /// @return s The storage instance of type `AppStorage`.
+    function _s() internal pure returns (AppStorage storage s) {
         return LibAppStorage.diamondStorage();
-     }
-    
+    }
 }
--- contracts/libraries/LibAppStorage.sol
+++ contracts/libraries/LibAppStorage.sol
@@ -1,103 +1,80 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 pragma solidity ^0.8.23;
 
-/**
- * @title LibAppStorage
- * @author Juan Luis Ramos Villalón
- * @author Luis de la Torre Cubillo
- * @dev This library defines the main storage structure (AppStorage) used in the Diamond pattern,
- * following the EIP-2535 specification. It enables centralized and secure storage that can be accessed
- * by multiple facets of the contract without memory slot collisions.
- *
- * AppStorage is used to declare shared variables across the modular architecture of the contract,
- * facilitating state management, access control, configuration, and other cross-facet functionalities.
- *
- * This library is essential for maintaining consistency and efficiency in Diamond contracts.
- */
-
 import {EnumerableSet} from "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
 
-/**
- * @dev Constant representing the hash of the string "APP_STORAGE_POSITION".
- *      This is used as a unique identifier for the application storage position.
- */
-bytes32 constant APP_STORAGE_POSITION = keccak256("diamond.standard.app.storage");
+/// @title LibAppStorage
+/// @author Juan Luis Ramos Villalón
+/// @author Luis de la Torre Cubillo
+/// @dev This library defines the main storage structure (AppStorage) used in the Diamond pattern,
+/// following the EIP-2535 specification. It enables centralized and secure storage that can be accessed
+/// by multiple facets of the contract without memory slot collisions.
+///
+/// AppStorage is used to declare shared variables across the modular architecture of the contract,
+/// facilitating state management, access control, configuration, and other cross-facet functionalities.
+///
+/// This library is essential for maintaining consistency and efficiency in Diamond contracts.
 
-/**
- * @dev Constant representing the hash of the string "PROVIDER_ROLE".
- *      This is used as a unique identifier for the provider role within the contract.
- */
+/// @dev Constant representing the hash of the string "APP_STORAGE_POSITION".
+///      This is used as a unique identifier for the application storage position.
+bytes32 constant APP_STORAGE_POSITION = keccak256(
+    "diamond.standard.app.storage"
+);
+
+/// @dev Constant representing the hash of the string "PROVIDER_ROLE".
+///      This is used as a unique identifier for the provider role within the contract.
 bytes32 constant PROVIDER_ROLE = keccak256("PROVIDER_ROLE");
 
-  
-/**
- * @dev Struct representing a Lab Provider.
- * @param name The name of the Lab Provider.
- * @param email The email address of the Lab Provider.
- * @param country The country of the Lab Provider.
- */
+/// @dev Struct representing a Lab Provider.
+/// @param name The name of the Lab Provider.
+/// @param email The email address of the Lab Provider.
+/// @param country The country of the Lab Provider.
 struct ProviderBase {
-        string name;
-        string email;
-        string country;
-    }
-
-/**
- * @dev Struct representing an extended Lab provider.
- * @param account The address of the Lab provider.
- * @param base The base Lab provider information.
- */
-struct Provider {
-        address account;
-        ProviderBase base;
-    }
-
-
-/**
- * @title AppStorage
- * @dev This struct is used to define the storage layout for the application.
- * It contains the following fields:
- * 
- * @param DEFAULT_ADMIN_ROLE A bytes32 value representing the default admin role.
- * @param labTokenAddress The address of the lab token contract.
- * @param roleMembers A mapping that associates a role (bytes32) with a set of addresses (EnumerableSet.AddressSet)
- *                    representing the members of that role.
- * @param providers A mapping that associates an address with a ProviderBase structure, representing the providers.
- */
-struct AppStorage {
-
-         bytes32 DEFAULT_ADMIN_ROLE;
-
-         address labTokenAddress;
-
-         mapping(bytes32 role => EnumerableSet.AddressSet) roleMembers;
-        
-         mapping(address => ProviderBase) providers;            
-       
+    string name;
+    string email;
+    string country;
 }
 
+/// @dev Struct representing an extended Lab provider.
+/// @param account The address of the Lab provider.
+/// @param base The base Lab provider information.
+struct Provider {
+    address account;
+    ProviderBase base;
+}
 
-/**
- * @title LibAppStorage
- * @dev A library that provides a function to access the application storage for a diamond standard contract.
- * 
- * @notice This library is used to retrieve the storage slot for the application state.
- * 
- * @function diamondStorage
- * @dev Retrieves the application storage by returning a reference to the `AppStorage` struct.
- *      The storage slot is determined by the constant `APP_STORAGE_POSITION`.
- * @return ds A storage pointer to the `AppStorage` struct.
- * 
- * @notice This function uses inline assembly to set the storage slot for the application state.
- */
+/// @title AppStorage
+/// @dev This struct is used to define the storage layout for the application.
+/// It contains the following fields:
+///
+/// @param DEFAULT_ADMIN_ROLE A bytes32 value representing the default admin role.
+/// @param labTokenAddress The address of the lab token contract.
+/// @param roleMembers A mapping that associates a role (bytes32) with a set of addresses (EnumerableSet.AddressSet)
+///                    representing the members of that role.
+/// @param providers A mapping that associates an address with a ProviderBase structure, representing the providers.
+struct AppStorage {
+    bytes32 DEFAULT_ADMIN_ROLE;
+    address labTokenAddress;
+    mapping(bytes32 role => EnumerableSet.AddressSet) roleMembers;
+    mapping(address => ProviderBase) providers;
+}
+
+/// @title LibAppStorage
+/// @dev A library that provides a function to access the application storage for a diamond standard contract.
+///
+/// @notice This library is used to retrieve the storage slot for the application state.
+///
+/// @function diamondStorage
+/// @dev Retrieves the application storage by returning a reference to the `AppStorage` struct.
+///      The storage slot is determined by the constant `APP_STORAGE_POSITION`.
+/// @return ds A storage pointer to the `AppStorage` struct.
+///
+/// @notice This function uses inline assembly to set the storage slot for the application state.
 library LibAppStorage {
-  
     function diamondStorage() internal pure returns (AppStorage storage ds) {
         bytes32 position = APP_STORAGE_POSITION;
         assembly {
             ds.slot := position
-            }
+        }
     }
-
-  
 }
